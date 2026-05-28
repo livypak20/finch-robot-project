fa80aeffcda551f3e09e53fbd2c61187333cf6db
livypak <livypak@gmail.com>
2026-05-28 14:29:44 -0700

    init commit (day 1 and 2)

---
+++ src/main/java/com/oliviaandjolie/TesterFinch.java
@@ -0,0 +1,11 @@
+package com.oliviaandjolie;
+
+public class TesterFinch {
+    public static void main(String[] args){
+        FinalFinch sonion = new FinalFinch("A");
+        
+        sonion.setMove("F", 10, 60);
+    }
+
+
+}
