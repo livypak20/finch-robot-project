67574e1f5f0e714f92a36b493d5520dba8e069d6
livypak <livypak@gmail.com>
2026-05-29 14:22:05 -0700

    day 3 - starting the drawing (house)

--- src/main/java/com/oliviaandjolie/TesterFinch.java
+++ src/main/java/com/oliviaandjolie/TesterFinch.java
@@ -4,7 +4,35 @@ public class TesterFinch {
     public static void main(String[] args){
         FinalFinch sonion = new FinalFinch("A");
         
-        sonion.setMove("F", 10, 60);
+        int speed = 10;
+
+//draws house base
+        for(int i = 0; i < 4; i++){
+            sonion.setMove("F", 9, speed);
+            sonion.setTurn("R", 90, speed);
+        }
+
+        sonion.setTurn("R", 90, speed);
+        sonion.setMove("F", 3, speed);
+        sonion.setTurn("L", 90, speed);
+        sonion.setMove("F", 3, speed);
+        sonion.setTurn("R", 90, speed);
+        sonion.setMove("F", 3, speed);
+        sonion.setTurn("R", 90, speed);
+        sonion.setMove("F", 3, speed);
+
+        sonion.setTurn("L", 90, speed);
+        sonion.setMove("F", 3, speed);
+        sonion.setTurn("L", 90, speed);
+        sonion.setMove("F", 9, speed);
+
+        sonion.setTurn("L", 45, speed);
+        sonion.setMove("F", 5, speed);
+
+
+
+
+
     }
 
 
