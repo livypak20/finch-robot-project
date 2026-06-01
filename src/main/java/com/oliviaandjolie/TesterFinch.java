934e3998fbe54cbcc1bedb76424549c94553c75c
livypak <livypak@gmail.com>
2026-06-01 14:22:25 -0700

    day 3 (finished the drawing)

--- src/main/java/com/oliviaandjolie/TesterFinch.java
+++ src/main/java/com/oliviaandjolie/TesterFinch.java
@@ -20,14 +20,27 @@ public class TesterFinch {
         sonion.setMove("F", 3, speed);
         sonion.setTurn("R", 90, speed);
         sonion.setMove("F", 3, speed);
-
         sonion.setTurn("L", 90, speed);
         sonion.setMove("F", 3, speed);
         sonion.setTurn("L", 90, speed);
         sonion.setMove("F", 9, speed);
 
+        //roof
         sonion.setTurn("L", 45, speed);
-        sonion.setMove("F", 5, speed);
+        sonion.setMove("F", 6, speed);
+        sonion.setTurn("L", 85, speed);
+        sonion.setMove("F", 6, speed); 
+
+        //chimney
+        sonion.setTurn("R", 180, speed);
+        sonion.setMove("F", 1.5, speed);
+        sonion.setTurn("L", 45, speed);
+        sonion.setMove("F", 3, speed);
+        sonion.setTurn("R", 90, speed);
+        sonion.setMove("F", 2, speed);
+        sonion.setTurn("R", 90, speed);
+        sonion.setMove("F", 1.5, speed);
+
 
 
 
