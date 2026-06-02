2a4e97c5770082ceb86aad5fb2cd3d5466505250
livypak <livypak@gmail.com>
2026-06-02 13:45:14 -0700

    day 4 (getting all the technical requirements)

--- src/main/java/com/oliviaandjolie/FinalFinch.java
+++ src/main/java/com/oliviaandjolie/FinalFinch.java
@@ -3,7 +3,62 @@ package com.oliviaandjolie;
 import com.birdbrain.Finch;
 
 public class FinalFinch extends Finch{
+
+        //array for most common values   
+        static int nums[] = {10, 90, 3};
+    //boolean to check if program finished
+        static boolean isFinished = false;
+
     public FinalFinch(String device){
         super(device);
     }
+
+    public void drawHouse(){
+            //variables for most common values
+            int speed = nums[0];
+            int rightAngle = nums[1];
+            int shortDistance = nums[2];
+
+            //draws house base
+            for(int i = 0; i < 4; i++){
+                setMove("F", 9, speed);
+                setTurn("R", rightAngle, speed);
+            }
+
+            //draws house door
+            setTurn("R", rightAngle, speed);
+            setMove("F", shortDistance, speed);
+            setTurn("L", rightAngle, speed);
+            setMove("F", shortDistance, speed);
+            setTurn("R", rightAngle, speed);
+            setMove("F", shortDistance, speed);
+            setTurn("R", rightAngle, speed);
+            setMove("F", shortDistance, speed);
+            setTurn("L", rightAngle, speed);
+            setMove("F", shortDistance, speed);
+            setTurn("L", rightAngle, speed);
+            setMove("F", 9, speed);
+
+            //roof
+            setTurn("L", 45, speed);
+            setMove("F", 6, speed);
+            setTurn("L", 85, speed);
+            setMove("F", 6, speed); 
+
+            //chimney
+            setTurn("R", 180, speed);
+            setMove("F", 1.5, speed);
+            setTurn("L", 45, speed);
+            setMove("F", shortDistance, speed);
+            setTurn("R", rightAngle, speed);
+            setMove("F", 2, speed);
+            setTurn("R", rightAngle, speed);
+            setMove("F", 1.5, speed);
+            
+            // isFinished = true;
+
+            // if(isFinished == true){
+            //     setBeak(0, 100, 100);
+            // }
+        }
 }
--- src/main/java/com/oliviaandjolie/TesterFinch.java
+++ src/main/java/com/oliviaandjolie/TesterFinch.java
@@ -1,52 +1,14 @@
 package com.oliviaandjolie;
 
 public class TesterFinch {
+
     public static void main(String[] args){
         FinalFinch sonion = new FinalFinch("A");
-        
-        int speed = 10;
 
-//draws house base
-        for(int i = 0; i < 4; i++){
-            sonion.setMove("F", 9, speed);
-            sonion.setTurn("R", 90, speed);
-        }
+   
+        sonion.drawHouse();
 
-        sonion.setTurn("R", 90, speed);
-        sonion.setMove("F", 3, speed);
-        sonion.setTurn("L", 90, speed);
-        sonion.setMove("F", 3, speed);
-        sonion.setTurn("R", 90, speed);
-        sonion.setMove("F", 3, speed);
-        sonion.setTurn("R", 90, speed);
-        sonion.setMove("F", 3, speed);
-        sonion.setTurn("L", 90, speed);
-        sonion.setMove("F", 3, speed);
-        sonion.setTurn("L", 90, speed);
-        sonion.setMove("F", 9, speed);
-
-        //roof
-        sonion.setTurn("L", 45, speed);
-        sonion.setMove("F", 6, speed);
-        sonion.setTurn("L", 85, speed);
-        sonion.setMove("F", 6, speed); 
-
-        //chimney
-        sonion.setTurn("R", 180, speed);
-        sonion.setMove("F", 1.5, speed);
-        sonion.setTurn("L", 45, speed);
-        sonion.setMove("F", 3, speed);
-        sonion.setTurn("R", 90, speed);
-        sonion.setMove("F", 2, speed);
-        sonion.setTurn("R", 90, speed);
-        sonion.setMove("F", 1.5, speed);
-
-
-
-
-
-
+    
     }
-
 
 }
